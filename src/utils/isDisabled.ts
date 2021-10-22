f6b4ada0352adc2dbba68b7cac51bc607f91293e
YevgeniyMakkaveev <yevgenmakkav@gmail.com>
2021-10-22 17:40:19 +0300

    fix

--- src/utils/isDisabled.ts
+++ src/utils/isDisabled.ts
@@ -3,50 +3,47 @@ import Access from '../models/Access';
 import Status from '../models/Status';
 import Version from '../models/Version';
 
-
 const isDisabled = (
   selectedRole: Access = Access.VIEWER,
   version: Version
 ): boolean => {
   const { status, approvalStages } = version;
-  console.log(selectedRole === Access.DPP && approvalStages.dpp.acepted === false)
-  console.log(selectedRole === Access.DPP, `role`)
-  console.log(approvalStages.dpp.acepted === false, `stage`)
+
   if (!status) {
     return true;
   }
-
-  switch (status) {
-    case Status.SCATCH:
-    case Status.REFACTORING:
-       return !(selectedRole === Access.EDITOR);
-    
-    case Status.APPROVING:
-      if (selectedRole === Access.DPP && approvalStages.dpp.acepted === false) {
-        return false;
-      }
-      if (selectedRole === Access.UIB && approvalStages.uib.acepted === false&&approvalStages.dpp.acepted === true) {
-        return false;
-      }
-       if (
-        selectedRole === Access.DISPATCH &&
-        approvalStages.fku.acepted === false && approvalStages.dpp.acepted === true&&
+  switch (selectedRole) {
+    case Access.EDITOR:
+      return !(status === Status.SCATCH || status === Status.REFACTORING);
+    case Access.DPP:
+      return !(
+        status === Status.APPROVING && approvalStages.dpp.acepted === false
+      );
+    case Access.UIB:
+      return !(
+        status === Status.APPROVING &&
+        approvalStages.uib.acepted === false &&
+        approvalStages.dpp.acepted === true
+      );
+    case Access.DISPATCH:
+      return !(
+        status === Status.APPROVING &&
+        approvalStages.fku.acepted === false &&
+        approvalStages.dpp.acepted === true &&
         !approvalStages.fku.reviwerRole
-      ) {
-        return false;
-      }
-      if (
+      );
+    case Access.UIT:
+      return !(
+        status === Status.APPROVING &&
+        approvalStages.uit.acepted === false &&
+        approvalStages.fku.acepted === true
+      );
+    default:
+      return !(
+        status === Status.APPROVING &&
         selectedRole === approvalStages.fku.reviwerRole &&
         approvalStages.fku.acepted === false
-      ) {
-        return false;
-      }
-       if (selectedRole === Access.UIT && approvalStages.uit.acepted === false&&approvalStages.fku.acepted === true) {
-        return false;
-      }
-      return true;
-    default:
-      return true;
+      );
   }
 };
 export default isDisabled;
